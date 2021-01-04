fc1d5e4b89990d5408e6b93e31439dea341fa003
joshkohane <joshkohane@gmail.com>
2021-01-04 18:55:23 -0500

    Add click events

--- src/index.js
+++ src/index.js
@@ -6,9 +6,21 @@ document.addEventListener("DOMContentLoaded", () => {
     const canvas = document.getElementById('game-canvas');
     const gameStartBtn = document.getElementById('game-start-btn');
     const gameStart = document.getElementById('game-start-container');
+    const gameWinBtn = document.getElementById('game-win-btn');
+    const gameWin = document.getElementById('game-win-container');
+    const gameLoseBtn = document.getElementById('game-lose-btn');
+    const gameLose = document.getElementById('game-lose-container');
 
 
     gameStartBtn.addEventListener('click', () => {
         gameStart.classList.add('hidden')
+    })
+
+    gameWinBtn.addEventListener('click', () => {
+        gameWin.classList.add('hidden')
+    })
+
+    gameLoseBtn.addEventListener('click', () => {
+        gameLose.classList.add('hidden')
     })
 })
