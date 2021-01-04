5ac6f398707a0b8ffd108930743642a0df937318
joshkohane <joshkohane@gmail.com>
2021-01-04 17:39:33 -0500

    Set up webpack and landing page

---
+++ src/index.js
@@ -0,0 +1,8 @@
+console.log('WEBPACK IS WORKING')
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    console.log('WEBPACK');
+    const canvas = document.getElementById('game-canvas');
+    // canvas.fillStyle = 'black';
+})
